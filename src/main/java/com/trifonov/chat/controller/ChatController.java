563dc3528955347fe87a72028a35fe9cdb78a57a
Ivaylo <itrifonov87@gmail.com>
2024-06-14 13:19:52 +0200

    Chat app base stage.

---
+++ src/main/java/com/trifonov/chat/controller/ChatController.java
@@ -0,0 +1,14 @@
+package com.trifonov.chat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @GetMapping("/")
+    public String showChatPAge() {
+        return "chat";
+    }
+
+}
